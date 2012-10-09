787e1bb7307afa06096eaca55f05659095cafd5e
Krzysztof Rutka <krzysztof.rutka@erlang-solutions.com>
2012-10-09 14:15:10 +0200

    Fix: Forgot controller table config

--- src/ofp_v3_enum.erl
+++ src/ofp_v3_enum.erl
@@ -26,31 +26,31 @@
 %%------------------------------------------------------------------------------
 
 -enum({type, [hello,
-	      error,
-	      echo_request,
-	      echo_reply,
-	      experimenter,
-	      features_request,
-	      features_reply,
-	      get_config_request,
-	      get_config_reply,
-	      set_config,
-	      packet_in,
-	      flow_removed,
-	      port_status,
-	      packet_out,
-	      flow_mod,
-	      group_mod,
-	      port_mod,
-	      table_mod,
-	      stats_request,
-	      start_reply,
-	      barrier_request,
-	      barrier_reply,
-	      queue_get_config_request,
-	      queue_get_config_reply,
-	      role_request,
-	      role_reply]}).
+              error,
+              echo_request,
+              echo_reply,
+              experimenter,
+              features_request,
+              features_reply,
+              get_config_request,
+              get_config_reply,
+              set_config,
+              packet_in,
+              flow_removed,
+              port_status,
+              packet_out,
+              flow_mod,
+              group_mod,
+              port_mod,
+              table_mod,
+              stats_request,
+              start_reply,
+              barrier_request,
+              barrier_reply,
+              queue_get_config_request,
+              queue_get_config_reply,
+              role_request,
+              role_reply]}).
 
 %%------------------------------------------------------------------------------
 %% Common Structures
@@ -68,30 +68,30 @@
                     live]}).
 
 -enum({port_no, [{in_port, 16#ffffff00},
-		 {table, 16#fffffff8},
-		 normal,
-		 flood,
-		 all,
-		 controller,
-		 local,
-		 any]}).
+                 {table, 16#fffffff8},
+                 normal,
+                 flood,
+                 all,
+                 controller,
+                 local,
+                 any]}).
 
 -enum({port_features, ['10mb_hd',
-		       '10mb_fd',
-		       '100mb_hd',
-		       '100mb_fd',
-		       '1gb_hd',
-		       '1gb_fd',
-		       '10gb_fd',
-		       '40gb_fd',
-		       '100gb_fd',
-		       '1tb_fd',
-		       other,
-		       copper,
-		       fiber,
-		       autoneg,
-		       pause,
-		       pause_asym]}).
+                       '10mb_fd',
+                       '100mb_hd',
+                       '100mb_fd',
+                       '1gb_hd',
+                       '1gb_fd',
+                       '10gb_fd',
+                       '40gb_fd',
+                       '100gb_fd',
+                       '1tb_fd',
+                       other,
+                       copper,
+                       fiber,
+                       autoneg,
+                       pause,
+                       pause_asym]}).
 
 %% Queue Structures ------------------------------------------------------------
 
@@ -99,85 +99,85 @@
 -enum({queue, [{all, 16#ffffffff}]}).
 
 -enum({queue_properties, [min_rate,
-			  max_rate,
-			  {experimenter, 16#ffff}]}).
+                          max_rate,
+                          {experimenter, 16#ffff}]}).
 
 %% Flow Match Structures -------------------------------------------------------
 
 -enum({match_type, [standard,
-		    oxm]}).
+                    oxm]}).
 
 -enum({oxm_class, [nxm_0,
-		   nxm_1,
-		   {openflow_basic, 16#8000},
-		   {experimenter, 16#ffff}]}).
+                   nxm_1,
+                   {openflow_basic, 16#8000},
+                   {experimenter, 16#ffff}]}).
 
 -enum({oxm_ofb_match_fields, [in_port,
-			      in_phy_port,
-			      metadata,
-			      eth_dst,
-			      eth_src,
-			      eth_type,
-			      vlan_vid,
-			      vlan_pcp,
-			      ip_dscp,
-			      ip_ecn,
-			      ip_proto,
-			      ipv4_src,
-			      ipv4_dst,
-			      tcp_src,
-			      tcp_dst,
-			      udp_src,
-			      udp_dst,
-			      sctp_src,
-			      sctp_dst,
-			      icmpv4_type,
-			      icmpv4_code,
-			      arp_op,
-			      arp_spa,
-			      arp_tpa,
-			      arp_sha,
-			      arp_tha,
-			      ipv6_src,
-			      ipv6_dst,
-			      ipv6_flabel,
-			      icmpv6_type,
-			      icmpv6_code,
-			      ipv6_nd_target,
-			      ipv6_nd_sll,
-			      ipv6_nd_tll,
-			      mpls_label,
-			      mpls_tc]}).
+                              in_phy_port,
+                              metadata,
+                              eth_dst,
+                              eth_src,
+                              eth_type,
+                              vlan_vid,
+                              vlan_pcp,
+                              ip_dscp,
+                              ip_ecn,
+                              ip_proto,
+                              ipv4_src,
+                              ipv4_dst,
+                              tcp_src,
+                              tcp_dst,
+                              udp_src,
+                              udp_dst,
+                              sctp_src,
+                              sctp_dst,
+                              icmpv4_type,
+                              icmpv4_code,
+                              arp_op,
+                              arp_spa,
+                              arp_tpa,
+                              arp_sha,
+                              arp_tha,
+                              ipv6_src,
+                              ipv6_dst,
+                              ipv6_flabel,
+                              icmpv6_type,
+                              icmpv6_code,
+                              ipv6_nd_target,
+                              ipv6_nd_sll,
+                              ipv6_nd_tll,
+                              mpls_label,
+                              mpls_tc]}).
 
 -enum({vlan_id, [none,
-		 {present, 16#1000}]}).
+                 {present, 16#1000}]}).
 
 %% Flow Instruction Structures -------------------------------------------------
 
 -enum({instruction_type, [{goto_table, 1},
-			  write_metadata,
-			  write_actions,
-			  apply_actions,
-			  clear_actions,
-			  {experimenter, 16#ffff}]}).
+                          write_metadata,
+                          write_actions,
+                          apply_actions,
+                          clear_actions,
+                          {experimenter, 16#ffff}]}).
 
 %% Action Structures -----------------------------------------------------------
 
 -enum({action_type, [output,
-		     {copy_ttl_out, 11},
-		     copy_ttl_in,
-		     {set_mpls_ttl, 15},
-		     dec_mpls_ttl,
-		     push_vlan,
-		     pop_vlan,
-		     push_mpls,
-		     pop_mpls,
-		     set_queue,
-		     group,
-		     set_nw_ttl,
-		     dec_nw_ttl,
-		     set_field,
-		     {experimenter, 16#ffff}]}).
+                     {copy_ttl_out, 11},
+                     copy_ttl_in,
+                     {set_mpls_ttl, 15},
+                     dec_mpls_ttl,
+                     push_vlan,
+                     pop_vlan,
+                     push_mpls,
+                     pop_mpls,
+                     set_queue,
+                     group,
+                     set_nw_ttl,
+                     dec_nw_ttl,
+                     set_field,
+                     {experimenter, 16#ffff}]}).
 
 %%------------------------------------------------------------------------------
 %% Controller-to-Switch Messages
@@ -186,25 +186,26 @@
 %% Handshake -------------------------------------------------------------------
 
 -enum({capabilities, [flow_stats,
-		      table_stats,
-		      port_stats,
-		      group_stats,
-		      {ip_reasm, 5},
-		      queue_stats,
-		      {port_blocked, 8}]}).
+                      table_stats,
+                      port_stats,
+                      group_stats,
+                      {ip_reasm, 5},
+                      queue_stats,
+                      {port_blocked, 8}]}).
 
 %% Switch Configuration --------------------------------------------------------
 
 -enum({config_flags, [frag_drop,
-		      frag_reasm,
-		      invalid_ttl_to_controller]}).
+                      frag_reasm,
+                      invalid_ttl_to_controller]}).
 
 %% Flow Table Configuration ----------------------------------------------------
 
 -enum({table, [{all, 16#ff}]}).
 
--enum({table_config, [continue,
-		      drop]}).
+-enum({table_config, [controller,
+                      continue,
+                      drop]}).
 
 %% Modify State Messages -------------------------------------------------------
 
@@ -212,27 +213,27 @@
 -enum({buffer, [{no_buffer, 16#ffff}]}).
 
 -enum({flow_mod_command, [add,
-			  modify,
-			  modify_strict,
-			  delete,
-			  delete_strict]}).
+                          modify,
+                          modify_strict,
+                          delete,
+                          delete_strict]}).
 
 -enum({flow_mod_flags, [send_flow_rem,
-			check_overlap,
-			reset_counts]}).
+                        check_overlap,
+                        reset_counts]}).
 
 %% Note: Not in the specification
 -enum({group, [{any, 16#fffffffe},
-	       all]}).
+               all]}).
 
 -enum({group_mod_command, [add,
-			   modify,
-			   delete]}).
+                           modify,
+                           delete]}).
 
 -enum({group_type, [all,
-		    select,
-		    indirect,
-		    ff]}).
+                    select,
+                    indirect,
+                    ff]}).
 
 %% Read State Messages ---------------------------------------------------------
 
@@ -241,20 +242,20 @@
 -enum({stats_reply_flags, [{more, 1}]}).
 
 -enum({stats_type, [desc,
-		    flow,
-		    aggregate,
-		    table,
-		    port,
-		    queue,
-		    group,
-		    group_desc,
-		    group_features,
-		    {experimenter, 16#ffff}]}).
+                    flow,
+                    aggregate,
+                    table,
+                    port,
+                    queue,
+                    group,
+                    group_desc,
+                    group_features,
+                    {experimenter, 16#ffff}]}).
 
 -enum({group_capabilities, [select_weight,
-			    select_liveness,
-			    chaining,
-			    chaining_checks]}).
+                            select_liveness,
+                            chaining,
+                            chaining_checks]}).
 
 %% Queue Configuration Messages ------------------------------------------------
 
@@ -265,9 +266,9 @@
 %% Role Request Messages -------------------------------------------------------
 
 -enum({controller_role, [nochange,
-			 equal,
-			 master,
-			 slave]}).
+                         equal,
+                         master,
+                         slave]}).
 
 %%------------------------------------------------------------------------------
 %% Asynchronous Messages
@@ -276,37 +277,37 @@
 %% Packet-In Message -----------------------------------------------------------
 
 -enum({packet_in_reason, [no_match,
-			  action,
-			  invalid_ttl]}).
+                          action,
+                          invalid_ttl]}).
 
 %% Flow Removed Message --------------------------------------------------------
 
 -enum({flow_removed_reason, [idle_timeout,
-			     hard_timeout,
-			     delete,
-			     group_delete]}).
+                             hard_timeout,
+                             delete,
+                             group_delete]}).
 
 %% Port Status Message ---------------------------------------------------------
 
 -enum({port_reason, [add,
-		     delete,
-		     modify]}).
+                     delete,
+                     modify]}).
 
 %% Error Message ---------------------------------------------------------------
 
 -enum({error_type, [hello_failed,
-		    bad_request,
-		    bad_action,
-		    bad_instruction,
-		    bad_match,
-		    flow_mod_failed,
-		    group_mod_failed,
-		    port_mod_failed,
-		    table_mod_failed,
-		    queue_op_failed,
-		    switch_config_failed,
-		    role_request_failed,
-		    {experimenter, 16#ffff}]}).
+                    bad_request,
+                    bad_action,
+                    bad_instruction,
+                    bad_match,
+                    flow_mod_failed,
+                    group_mod_failed,
+                    port_mod_failed,
+                    table_mod_failed,
+                    queue_op_failed,
+                    switch_config_failed,
+                    role_request_failed,
+                    {experimenter, 16#ffff}]}).
 
 -enum({hello_failed, [incompatible,
                       eperm]}).
